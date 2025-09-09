58aa0ab9097cded8399e36bbfd28e8e72ceed2ee
48CODER84 <99927084+48CODER84@users.noreply.github.com>
2025-09-09 11:43:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -140,9 +140,10 @@ function logs(){
       <li>Canvas Security</li>
       <li>Added Games</li>
       </ul>
-      <h3>2.3</h3>
+      <h3>2.3 - In Progress</h3>
       <ul>
       <li>Added Hollow Knight</li>
+      <li>Added Polytrack</li>
       </ul>
       <h3>2.2</h3>
       <ul>
